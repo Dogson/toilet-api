45464db6e0ad1cde7ca540cde91f76661b51bb84
Dogson <gwenael.girod@gmail.com>
2018-09-14 12:40:07 +0000

    Added basic authentication

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,8 +1,9 @@
 'use strict';
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
 
-let UserSchema = new Schema({
+const UserSchema = new Schema({
     username: {
         type: String,
         required: "username must be specified"
@@ -17,5 +18,30 @@ let UserSchema = new Schema({
         unique: true
     }
 });
+
+UserSchema.pre('save', function(callback) {
+    let user = this;
+
+    // Break out if the password hasn't changed
+    if (!user.isModified('password')) return callback();
+
+    // Password changed so we need to hash it
+    bcrypt.genSalt(5, function(err, salt) {
+        if (err) return callback(err);
+
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) return callback(err);
+            user.password = hash;
+            callback();
+        });
+    });
+});
+
+UserSchema.methods.verifyPassword = function(password, cb) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema);
